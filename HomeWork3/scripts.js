17c4f61dd7c9ba44a460db7a7ad219d633f6d8fc
Sandeep Vanga <sandeepvanga@sandeeps-MacBook-Pro.local>
2015-04-24 22:51:15 -0700

    Adding Area plot and zooming functionality

--- HomeWork3/scripts.js
+++ HomeWork3/scripts.js
@@ -1,5 +1,5 @@
 function multiseries_line(id,h,w) {
-var margin = {top: 40, right: 80, bottom: 30, left: 70},
+var margin = {top: 40, right: 100, bottom: 30, left: 100},
     width = w - margin.left - margin.right,
     height = h - margin.top - margin.bottom;
 
@@ -109,11 +109,11 @@ color.domain(d3.keys(data[0]).filter(function(key) { return (key == "DriverDeath
       .call(d3.legend);
 
   svg.append("text")
-      .attr("x", (w / 2)-70)             
+      .attr("x", (w / 2)-60)             
       .attr("y", 0 - (margin/2))
       .attr("text-anchor", "middle")  
-      .style("font-size", "30px") 
-      .text("Driver, Front-seat and Back-seat passenger Deaths/Injuries");
+      .style("font-size", "25px") 
+      .text("Time series of Driver, Front-seat and Back-seat passenger Deaths/Injuries");
 
   //Add a y-axis label.
   svg.append("text")
@@ -128,9 +128,132 @@ color.domain(d3.keys(data[0]).filter(function(key) { return (key == "DriverDeath
 });                    
 }
 
+function stacked_area(id,h,w) {
+
+var margin = {top: 100, right: 10, bottom: 100, left: 40},
+    margin2 = {top: 430, right: 10, bottom: 20, left: 40},
+    width = w - margin.left - margin.right,
+    height = h - margin.top - margin.bottom,
+    height2 = h - margin2.top - margin2.bottom;
+
+var parseDate = d3.time.format("%b %Y").parse;
+
+var x = d3.time.scale().range([0, width]),
+    x2 = d3.time.scale().range([0, width]),
+    y = d3.scale.linear().range([height, 0]),
+    y2 = d3.scale.linear().range([height2, 0]);
+
+var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+    xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
+    yAxis = d3.svg.axis().scale(y).orient("left");
+
+var brush = d3.svg.brush()
+    .x(x2)
+    .on("brush", brushed);
+
+var area = d3.svg.area()
+    .interpolate("monotone")
+    .x(function(d) { return x(d.date); })
+    .y0(height)
+    .y1(function(d) { return y(d.PetrolPrice); });
+
+var area2 = d3.svg.area()
+    .interpolate("monotone")
+    .x(function(d) { return x2(d.date); })
+    .y0(height2)
+    .y1(function(d) { return y2(d.PetrolPrice); });
+
+var svg = d3.selectAll("#"+id).select("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
 
 
-function bubble_chart(id,height,width) {
+svg.append("defs").append("clipPath")
+    .attr("id", "clip")
+  .append("rect")
+    .attr("width", width)
+    .attr("height", height);
+
+var focus = svg.append("g")
+    .attr("class", "focus")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var context = svg.append("g")
+    .attr("class", "context")
+    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+d3.csv("seatBelts.csv", type, function(error, data) {
+  x.domain(d3.extent(data.map(function(d) { return d.date; })));
+  y.domain([0, d3.max(data.map(function(d) { return d.PetrolPrice; }))]);
+  x2.domain(x.domain());
+  y2.domain(y.domain());
+
+  focus.append("path")
+      .datum(data)
+      .attr("class", "area")
+      .attr("d", area);
+
+  focus.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  focus.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+  context.append("path")
+      .datum(data)
+      .attr("class", "area")
+      .attr("d", area2);
+
+  context.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height2 + ")")
+      .call(xAxis2);
+
+  context.append("g")
+      .attr("class", "x brush")
+      .call(brush)
+    .selectAll("rect")
+      .attr("y", -6)
+      .attr("height", height2 + 7);
+
+  svg.append("text")
+      .attr("x", (w / 2)+20)             
+      .attr("y", 70)
+      .attr("text-anchor", "middle")  
+      .style("font-size", "25px") 
+      .text("Time series of Petrol Price");
+
+  //Add a y-axis label.
+  svg.append("text")
+  .attr("class", "y label")
+  .attr("text-anchor", "end")
+  .attr("y", 60)
+  .attr("x", -60)
+  //.attr("dy", ".75em")
+  .attr("transform", "rotate(-90)")
+  .text("Petrol Price");
+
+});
+
+function brushed() {
+  x.domain(brush.empty() ? x2.domain() : brush.extent());
+  focus.select(".area").attr("d", area);
+  focus.select(".x.axis").call(xAxis);
+}
+
+function type(d) {
+  d.date = parseDate(d.date);
+  d.PetrolPrice = +d.PetrolPrice;
+  return d;
+}
+
+}
+
+
+/*function bubble_chart(id,height,width) {
 
 // Various accessors that specify the four dimensions of data to visualize.
 function state(d) { return d.state_name; }
@@ -163,6 +286,9 @@ var w = width-(2*margin)
 function order(a, b) {
   return radius(b) - radius(a);
 }
+
+
+
 
 
 d3.csv("state.x77.csv", function(error, data){
@@ -277,4 +403,4 @@ d3.csv("state.x77.csv", function(error, data){
       .text("Illiteracy Vs Murder rate");
 
       });
-}
+}*/
